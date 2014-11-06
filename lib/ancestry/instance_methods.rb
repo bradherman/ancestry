4e5eb4c00920106c25f2fd192bc378e7e331a464
Bradley Herman <bradley.t.herman@gmail.com>
2014-11-06 14:10:31 -0800

    add set methods

--- lib/ancestry/instance_methods.rb
+++ lib/ancestry/instance_methods.rb
@@ -272,6 +272,51 @@ module Ancestry
       !!@disable_ancestry_callbacks
     end
 
+    # Accepts the typical array of ids from a scriptaculous sortable. It is called on the instance being moved
+    def sort(array_of_ids)
+      if array_of_ids.first == id.to_s
+        move_to_left_of siblings.find(array_of_ids.second)
+      else
+        move_to_right_of siblings.find(array_of_ids[array_of_ids.index(id.to_s) - 1])
+      end
+    end
+
+    def move_to_child_of(reference_instance)
+      transaction do
+        remove_from_list
+        self.update_attributes!(:parent => reference_instance)
+        add_to_list_bottom
+        save!
+      end
+    end
+
+    def move_to_left_of(reference_instance)
+      transaction do
+        remove_from_list
+        reference_instance.reload # Things have possibly changed in this list
+        self.update_attributes!(:parent_id => reference_instance.parent_id)
+        reference_item_position = reference_instance.position
+        increment_positions_on_lower_items(reference_item_position)
+        self.update_attribute(:position, reference_item_position)
+      end
+    end
+
+    def move_to_right_of(reference_instance)
+      transaction do
+        remove_from_list
+        reference_instance.reload # Things have possibly changed in this list
+        self.update_attributes!(:parent_id => reference_instance.parent_id)
+        if reference_instance.lower_item
+          lower_item_position = reference_instance.lower_item.position
+          increment_positions_on_lower_items(lower_item_position)
+          self.update_attribute(:position, lower_item_position)
+        else
+          add_to_list_bottom
+          save!
+        end
+      end   
+    end
+
   private
 
     def cast_primary_key(key)
