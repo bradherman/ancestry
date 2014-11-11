55fd8777400abf8be34e15e79109f66f4ab84f25
Bradley Herman <bradley.t.herman@gmail.com>
2014-11-10 16:44:02 -0800

    Mass assignment issue

--- lib/ancestry/list/instance_methods.rb
+++ lib/ancestry/list/instance_methods.rb
@@ -4,7 +4,7 @@ module Ancestry
       def move_to_child_of(reference_instance)
         transaction do
           remove_from_list
-          self.update_attributes!(:parent => reference_instance)
+          self.update_attribute(:parent, reference_instance)
           add_to_list_bottom
           save!
         end
@@ -14,7 +14,7 @@ module Ancestry
         transaction do
           remove_from_list
           reference_instance.reload # Things have possibly changed in this list
-          self.update_attributes!(:parent_id => reference_instance.parent_id)
+          self.update_attribute(:parent_id, reference_instance.parent_id)
           reference_item_position = reference_instance.position
           increment_positions_on_lower_items(reference_item_position)
           self.update_attribute(:position, reference_item_position)
@@ -25,7 +25,7 @@ module Ancestry
         transaction do
           remove_from_list
           reference_instance.reload # Things have possibly changed in this list
-          self.update_attributes!(:parent_id => reference_instance.parent_id)
+          self.update_attribute(:parent_id, reference_instance.parent_id)
           if reference_instance.lower_item
             lower_item_position = reference_instance.lower_item.position
             increment_positions_on_lower_items(lower_item_position)
